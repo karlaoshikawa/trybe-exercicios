a550e834a5355120255329a5e4f236a6ae1a2840
Karla Oshikawa <karlaoshikawa@iMac-de-Karla.local>
2022-07-21 07:08:06 +0900

    Exercicios js 1 - 2

---
+++ bloco-04-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -0,0 +1,28 @@
+/* 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, 
+a e b, definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)*/
+
+const a = 30;
+const b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+/* 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas 
+constantes com os valores que serão comparados.*/
+const maiorOuMenor1 = 120;
+const maiorOuMenor2 = 200;
+if (maiorOuMenor1 > maiorOuMenor2) {
+  console.log(maiorOuMenor1);
+} else {
+  console.log(maiorOuMenor2);
+}
+
+/* 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores 
+que serão comparados.
